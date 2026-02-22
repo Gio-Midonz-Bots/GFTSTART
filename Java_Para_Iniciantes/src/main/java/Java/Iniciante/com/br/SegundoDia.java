f11619dc55f188fcbc8907950b990cc86a221e49
Giovanna Midonz <roboticagiovanna@gmail.com>
2026-02-22 10:42:08 -0300

    Java Para Iniciante - Conceito Array Multidimensional

--- Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/SegundoDia.java
+++ Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/SegundoDia.java
@@ -25,9 +25,16 @@ public class SegundoDia {
         Arrays.sort(valores); // função de sort() organiza de menor para maior
 
         //A forma que o professor ensina -> [10, 15, 25, 2, 3, 54]
-        System.out.println(Arrays.toString(valores));
+        System.out.println(Arrays.toString(valores)); // Isso aqui converte o conteúdo de um array (valores) em uma string legível e a imprime no console. Então toda vez que declarar uma Array depois converta em string legível
         System.out.println(nomes.length);
         //ATENÇÃO: Arrays possui tamanho fixo e tipos primitivos e objetos
         // enquando ArrayList são dinamicas redimensiona-se automaticamente e armazenam apenas objetos
+        System.out.println("valor da lista indice 4: "+valores[4]);
+
+        //Array Multidimensional
+        //O difericial é os dois colçeites e as duas chaves e dividir
+        String[][] cars = {{"BMW", "Tesla","Jeep","Fiat"}, {"1998","2000","2022","1983"}};
+        //para imprimir utilize a deepToString()
+        System.out.println(Arrays.deepToString(cars));
     }
 }
