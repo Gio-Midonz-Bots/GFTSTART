2847ea23eee3f0b4990875b8f97aef06ef41bd10
Giovanna Midonz <roboticagiovanna@gmail.com>
2026-02-21 23:15:34 -0300

    Java Para Iniciante

--- Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/PrimeiroDia.java
+++ Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/PrimeiroDia.java
@@ -1,8 +1,12 @@
 package Java.Iniciante.com.br;
+import java.util.Scanner;
+import java.util.Scanner;
 
 public class PrimeiroDia {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         int numeroInteiro = 20000000; //o _ vai ajudar na divisão do numero também tem short, byte para usar como valor inteiro;
         long numeroLong = 3;
         float numeroFloat = 354;
@@ -15,6 +19,19 @@ public class PrimeiroDia {
 
         String nomeCompleto = "Georgia Midonz";
         System.out.println("Olá "+ nomeCompleto.toUpperCase());
-
+        System.out.println("Informe sua idade " +nomeCompleto);
+        int idade = sc.nextInt();
+        if (idade >= 18){
+            System.out.println(nomeCompleto.toUpperCase()+ " Vocé é maior de idade: "+idade);
+        }else {
+            System.out.println(nomeCompleto.toUpperCase()+ " Menor de idade: "+idade);
+        }
+        System.out.println("Projeto: Calculadora");
+        System.out.println("Informe um numero: ");
+        int numeroCalculador = sc.nextInt();
+        for (int i = 0 ; i < 10; i++){
+            int resultado = i * numeroCalculador;
+            System.out.println(i + " * " +numeroCalculador + " = " + resultado);
+        }
     }
 }
