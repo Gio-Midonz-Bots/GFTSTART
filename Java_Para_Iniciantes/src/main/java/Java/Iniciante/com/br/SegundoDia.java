54bec94bfb4e2f8f54b148e330d40788875c6fb5
Giovanna Midonz <roboticagiovanna@gmail.com>
2026-02-22 11:35:46 -0300

    Java Para Iniciante - dia 2 finalizado

--- Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/SegundoDia.java
+++ Java_Para_Iniciantes/src/main/java/Java/Iniciante/com/br/SegundoDia.java
@@ -36,5 +36,54 @@ public class SegundoDia {
         String[][] cars = {{"BMW", "Tesla","Jeep","Fiat"}, {"1998","2000","2022","1983"}};
         //para imprimir utilize a deepToString()
         System.out.println(Arrays.deepToString(cars));
+
+        //Constantes -> usamos final para declarar uma constante
+        final double taxaJuros = 11.92;
+        System.out.println(taxaJuros); //taxaJuros = 12;
+
+        //Operadores ->
+        //casting funçao
+        double a = (double) 4 / (double)3;
+
+        //Converte string em int -> utiliza Integer.parseInt()
+        String valorProduto = "10";
+        int valorTotal = Integer.parseInt(valorProduto) +2;
+        System.out.println(valorTotal);
+
+        //CLASS MAHT
+
+        double value = Math.round(10.4);//round arrendoda o numero
+        value = Math.ceil(12.6); // arrendoda para cima
+        value = Math.floor(11.2); // arrendoda para baixo
+        System.out.println(value);
+        //Numero aleatorio
+        int aleatorio = (int)Math.round(Math.random() * 100);
+        System.out.println(aleatorio);
+
+        //Projeto
+        int[] my_Array1 = {
+                1789, 2035,1899,1456,2013,
+                1458, 2458, 1254,1472,2365,
+                1456,2165,1457,2456
+        };
+
+        String[] my_Array2 = {
+                "Java",
+                "Python",
+                "PHP",
+                "Node",
+                "C#",
+                "C++",
+                "Javascript",
+                "Angular"
+        };
+        System.out.println("Array Original: "+Arrays.toString(my_Array1));
+        Arrays.sort(my_Array1);
+        System.out.println("Array Ordenada: "+Arrays.toString(my_Array1));
+
+        System.out.println("Array Original: "+ Arrays.toString(my_Array2));
+        Arrays.sort(my_Array2);
+        System.out.println("Array Ordenada: "+ Arrays.toString(my_Array2));
     }
 }
+
